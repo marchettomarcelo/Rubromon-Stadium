28cf23a5662502bfedc024b45805fedc82d4931d
Marcelo Marchetto <marcelomarchetto@MacBook-Pro-de-Marcelo.local>
2022-04-14 20:33:16 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const path = require("path")
+const app = express()
+
+const publicDirectoryPath = path.join(__dirname, "../public")
+app.use(express.static(publicDirectoryPath))
+
+const port = process.env.PORT || 3000
+
+app.get("/", (req, res)=>{
+    res.render("index.html")
+})
+
+app.listen(port, ()=>{
+    console.log("The server is up mennnnn!")
+})
