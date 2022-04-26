d3fdd99ef9c0e7de8b21d25040bd3e1265c80542
Marcelo Marchetto <marcelomarchetto@MacBook-Pro-de-Marcelo.local>
2022-04-26 15:06:12 -0300

    pages

--- src/app.js
+++ src/app.js
@@ -7,9 +7,23 @@ app.use(express.static(publicDirectoryPath))
 
 const port = process.env.PORT || 3000
 
-app.all("*", (req, res)=>{
+print(__dirname)
+
+app.get("/", (req, res)=>{
     res.render("index")
 })
+app.get("/heuristica", (req, res)=>{
+    var options = {
+        root: path.join(__dirname)
+    };
+    res.sendFile("index", options)
+})
+// app.get("/download", (req, res)=>{
+//     res.sendFile("index")
+// })
+// app.get("/dev-logs", (req, res)=>{
+//     res.sendFile("index")
+// })
 
 app.listen(port, ()=>{
     console.log("The server is up on http://localhost:3000/ mennnnn!")
